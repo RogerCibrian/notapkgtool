4dabebce1a7af90bd0ad4e92658679f8442084cd
RogerCibrian <70341939+RogerCibrian@users.noreply.github.com>
2025-05-14 02:59:49 +0000

    re-orged logic for http response handling

--- intunepackager/processors/download.py
+++ intunepackager/processors/download.py
@@ -8,10 +8,21 @@ def download_file(url: str, destination: Path) -> None:
     :param url: URL of the file to download.
     :param destination: Path where the file will be saved.
     """
-    response = requests.get(url, stream=True)
-    # raise error for bad HTTP status
-    response.raise_for_status()  
+    response = requests.get(url, stream=True, allow_redirects=False)
 
+    if 300 <= response.status_code < 400:
+        raise requests.exceptions.HTTPError(
+            f"Unexpected redirect ({response.status_code}) for URL: {url}",
+            response=response
+    )
+
+    # Raise error for non-200 status codes
+    if response.status_code != 200:
+        raise requests.exceptions.HTTPError(
+            f"Expected 200 OK but got {response.status_code}",
+            response=response
+        )
+    
     destination.parent.mkdir(parents=True, exist_ok=True)
     with destination.open("wb") as f:
         # Download in 8 KB chunks
