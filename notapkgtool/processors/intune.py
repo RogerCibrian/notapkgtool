5f86065c0c65268d2c7fd456eba1d8501bc9d9d3
RogerCibrian <70341939+RogerCibrian@users.noreply.github.com>
2025-06-05 15:06:10 -0700

    added methods

--- notapkgtool/processors/intune.py
+++ notapkgtool/processors/intune.py
@@ -3,8 +3,14 @@ def intune_client():
     Creates an Intune client using Graph API.
     """
 
-def get_intunepackager_apps ():
+def get_napt_apps ():
     """
     Retrieves a list of Intune apps published by Not a Pkg Tool.
     """
 
+def get_napt_app (app_id):
+    """
+    Retrieves a specific Intune app published by Not a Pkg Tool by its ID.
+    """
+
+def 
