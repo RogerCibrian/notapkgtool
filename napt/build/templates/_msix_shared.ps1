8fc7c579f010e95abdeaf5bb1fbe3f5a2e806e76
Roger Cibrian <roger.cibrian@outlook.com>
2026-04-02 21:50:21 -0700

    fix: Use script type for MSIX shared log category
    Replace [AppX] with [$NaptScriptType] in _msix_shared.ps1 to match
    the established log category pattern used by registry scripts.

--- napt/build/templates/_msix_shared.ps1
+++ napt/build/templates/_msix_shared.ps1
@@ -7,14 +7,14 @@ function Get-InstalledAppxPackage {
     try {
         $Package = Get-AppxPackage -Name $PackageIdentityName -ErrorAction Stop
         if ($Package) {
-            Write-CMTraceLog -Message "[AppX] Package found: $($Package.Name) (Version: $($Package.Version), Arch: $($Package.Architecture))" -Type "INFO"
+            Write-CMTraceLog -Message "[$NaptScriptType] Package found: $($Package.Name) (Version: $($Package.Version), Arch: $($Package.Architecture))" -Type "INFO"
             return $Package
         }
 
-        Write-CMTraceLog -Message "[AppX] Package not found: $PackageIdentityName" -Type "INFO"
+        Write-CMTraceLog -Message "[$NaptScriptType] Package not found: $PackageIdentityName" -Type "INFO"
         return $null
     } catch {
-        Write-CMTraceLog -Message "[AppX] Error querying package $PackageIdentityName : $($_.Exception.Message)" -Type "ERROR"
+        Write-CMTraceLog -Message "[$NaptScriptType] Error querying package $PackageIdentityName : $($_.Exception.Message)" -Type "ERROR"
         return $null
     }
 }
